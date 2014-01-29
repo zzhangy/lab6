cb43fbe87bf8177dbcd553bb387619684215e7a4
Michael Bernstein <msb@cs.stanford.edu>
2014-01-28 19:47:26 -0800

    initial commit of lab

---
+++ public/js/introHCI.js
@@ -0,0 +1,27 @@
+'use strict';
+
+// Call this function when the page loads (the "ready" event)
+$(document).ready(function() {
+	initializePage();
+})
+
+/*
+ * Function that is called when the document is ready.
+ */
+function initializePage() {
+	$('.project a').click(function(e) {
+		// Prevent following the link
+		e.preventDefault();
+
+		// Get the div ID, e.g., "project3"
+		var projectID = $(this).closest('.project').attr('id');
+		// get rid of 'project' from the front of the id 'project3'
+		var idNumber = projectID.substr('project'.length);
+
+		console.log("User clicked on project " + idNumber);
+	});
+
+	$('#colorBtn').click(function(e) {
+		console.log("User clicked on color button");
+	});
+}
---
+++ routes/project.js
@@ -0,0 +1,5 @@
+exports.viewProject = function(req, res) { 
+	var name = req.params.name;
+	console.log("The project name is: " + name);
+  	res.render('project', { 'projectName': name });
+}
