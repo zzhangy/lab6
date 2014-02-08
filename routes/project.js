369265ce7267fcf26ede1257560c7365003a40fb
Michael Bernstein <msb@cs.stanford.edu>
2014-02-08 13:52:44 -0800

    fixing off by one

--- routes/project.js
+++ routes/project.js
@@ -4,6 +4,6 @@ exports.projectInfo = function(req, res) { 
 	var projectID = parseInt(req.params.id);
 	console.log("The project ID is: " + projectID);
   	
-  	var project = projects[projectID];
+  	var project = projects[projectID-1]; // of by one, our first project has index 0
   	res.json(project);
 }
